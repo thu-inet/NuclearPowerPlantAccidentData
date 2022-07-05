16db453d4fa7624ba851181f2fcc47065d1fe50b
qiben-jy <qb@mails.tsinghua.edu.cn>
2022-07-05 22:51:59 +0800

    complete Pre_processing

--- Data Processing.py
+++ Data Processing.py
@@ -1,50 +1,10 @@
-# data_path = r".\\DATA"
-# for accident in os.listdir(data_path):
-#     accident_path = data_path + '\\' + accident
-#     print(accident_path)
-#     for name in os.listdir(accident_path):
-#         if re.match(r'\d+'+'.mdb', name): #operation data
-#             print('Operation data:', name)
-#         elif re.match(r'\d+' + 'dose' + '.mdb', name):  # Dose data
-#             print('dose data:', name)
-# mdb_file = r'.\\DATA\LOCA\1.mdb'
-# driver = '{Microsoft Access Driver (*.mdb, *.accdb)}'
-# cnxn = pyodbc.connect(f'Driver={driver};DBQ={mdb_file}')
-# crsr = cnxn.cursor()
-# for table_info in crsr.tables(tableType='TABLE'):
-#     print(table_info)
-# data_table = pd.read_sql('SELECT * FROM PlotData',cnxn)
-# data_table.sort_values(by=['TIME'], ascending=True, inplace=True) #Some mdbs have problems with not being in time order
-# print(data_table)
-# target_path = r'.\\CSV_DATA\LOCA'
-# if (os.path.exists(target_path) == False):
-#     os.makedirs(target_path)
-#     print(os.getcwd())
-# os.chdir(target_path)
-# data_table.to_csv('1.csv', header=True, index=False)
-# # variables = ['TIME','TFPK','TFSB','TPCT']
-# variables = ['TIME','HLW']
-# plot_df = data_table[variables]
-# plot_df.set_index(plot_df.columns[0],inplace=True)
-# # plot_df.sort_values(by=['TIME'], ascending=True, inplace=True)
-# print(plot_df)
-# fig_plot = plot_df.plot()
-# plt.xlabel('Time(sec)')
-# # plt.ylabel('Temperature(℃)')
-# # plt.ylabel('Pressure(bar)')
-# # plt.ylabel('Flow(t/hr)')
-# plt.ylabel('RCS leak (kJ/kg)')
-
-# plt.ylabel('Fraction Clad failure(%)')
-# plt.ylabel('Water level (M)')
-
-# plt.show()
-# fig_name = ''
-# for var in range(1,len(variables)):
-#     fig_name = fig_name  + variables[var] + '-'
-# print(fig_name)
-# fig_save = fig_plot.get_figure()
-# fig_save.savefig(f'.\\Data_figture\\{fig_name}')
+import pandas as pd
+import pyodbc, os, re
+import matplotlib.pyplot as plt
+import torch
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+from itertools import chain
 
 class Pre_processing:
     def __init__(self, data_path, operation_data_csv_path, dose_data_csv_path, project_path):
@@ -55,18 +15,17 @@ class Pre_processing:
     def mdbtocsv(self):
         driver = '{Microsoft Access Driver (*.mdb, *.accdb)}'
         if (os.path.exists(self.operation_data_csv_path) == False):
-            os.makedirs(self.operation_data_csv_path)
+            os.makedirs(self.operation_data_csv_path) #Create folder of operation parameters
         if (os.path.exists(self.dose_data_csv_path) == False):
-            os.makedirs(self.dose_data_csv_path)
+            os.makedirs(self.dose_data_csv_path)  #Create folder of dose parameters
         for accident in os.listdir(self.data_path):
             accident_path = self.data_path + '\\'+ accident
-            os.chdir(self.project_path)
+            os.chdir(self.project_path) # Make sure it is the project path
             for name in os.listdir(accident_path):
                 if not (re.match(r'\d+' + 'Transient Report.txt',name)):
-                    os.chdir(self.project_path)
+                    os.chdir(self.project_path) # Make sure the database conect normally
                     mdb_file = accident_path + '\\' + name
                     cnxn = pyodbc.connect(f'Driver={driver};DBQ={mdb_file}')
-                    # crsr = cnxn.cursor()
                     if re.match(r'\d+' + '.mdb', name): #Operation data
                         data_table = pd.read_sql('SELECT * FROM PlotData', cnxn)
                         data_table.sort_values(by=['TIME'], ascending=True,
@@ -87,22 +46,68 @@ class Pre_processing:
                         os.chdir(csv_accident_path)
                         csv_name = name.replace('mdb', 'csv')
                         data_table.to_csv(csv_name, header=True, index=False)
+    def generate_dataset(self, dataset_source_path):
+        class Mydataset(Dataset):
+            def __int__(self, dataset_path):
+                self.dataset_path = dataset_path
+                self.feature = []
+                self.label = []
+                """
+                1. Read all csv files in order
+                2. Add labels(accident types) to self.label, 
+                add features(operation data or dose data) to self.feature
+                """
+                for accident in os.listdir(self.dataset_path):
+                    accident_path = self.data_path + '\\' + accident
+                    for size_name in os.listdir(accident_path):
+                        csv_data_path = accident_path + '\\' + size_name
+                        self.label.append(accident)
+                        sample_df = pd.read_csv(csv_data_path)
+                        sample_value = (sample_df.iloc[:150, 1:]).values  # Take the data of 1500s
+                        sample_list = list(chain.from_iterable(sample_value))  # Convert 2-D list to 1-D list
+                        self.feature.append(sample_list)
+                self.label = (pd.Categorical(self.label)).codes
+                assert len(self.label) == len(self.feature)
+                self.length = len(self.feature)
+            def __getitem__(self, index):
+                x = self.feature[index]
+                x = torch.Tensor(x)
+                y = self.label[index]
+                return {"x": x, "y": y}
 
-    def generate_dataset(self):
-        pass
+            def __len__(self):
+                return self.length
+        return Mydataset(dataset_source_path)
+    def show_parametes(self, variables, plot_data_path, figture_save_path):
+        if (os.path.exists(figture_save_path) == False):
+            os.makedirs(figture_save_path)
+        plot_data = pd.read_csv(plot_data_path)
+        plot_df = plot_data[variables]
+        plot_df.set_index(plot_df.columns[0],inplace=True)
+        fig_plot = plot_df.plot()
+        plt.xlabel(variables[0])
+        plt.show()
+        fig_name = ''
+        for var in range(1,len(variables)):
+            fig_name = fig_name  + variables[var] + '-'
+        print(fig_name)
+        fig_save = fig_plot.get_figure()
+        fig_path = figture_save_path + "\\" + fig_name
+        fig_save.savefig(fig_path)
 
-    def show_parametes(self):
-        pass
 if __name__ == "__main__":
-    import pandas as pd
-    import pyodbc, os, re
-    import matplotlib.pyplot as plt
+
     pre_data = Pre_processing(r".\\DATA", r".\\Operation_csv_data",r".\\Dose_csv_data",os.getcwd())
-    pre_data.mdbtocsv()
-    # mdb_file = '.\\\\DATA\\ATWS\\1dose.mdb'
-    # driver = '{Microsoft Access Driver (*.mdb, *.accdb)}'
-    # print(os.getcwd())
-    # cnxn = pyodbc.connect(f'Driver={driver};DBQ={mdb_file}')
-    # crsr = cnxn.cursor()
-    # data_table = pd.read_sql('SELECT * FROM ListDS', cnxn)
-    # print(data_table)
+    # pre_data.mdbtocsv()
+    pre_data.show_parametes(['TIME','P'], r".\\Operation_csv_data\LOCA\1.csv", r".\\Data_figture\LOCA\1")
+
+    #test
+    # test_path = r'E:\software\PythonProject\pythonProject\NuclearPowerPlantAccidentData\Operation_csv_data\ATWS\1.csv'
+    # test_data = pd.read_csv(test_path)
+    # test_data1 = test_data.iloc[:150,1:].values
+    # test_data2 = list(chain.from_iterable(test_data1))
+    # print(len(test_data2))
+
+
+
+
